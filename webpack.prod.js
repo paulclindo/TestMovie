fe581ca36fce20ced47fdab7b2fcae17abd886a8
Paul Ccari <paulclindo@gmail.com>
2019-11-09 07:01:27 -0500

    Fixed some webpack config for production

---
+++ webpack.prod.js
@@ -0,0 +1,79 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TersetJSPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src/index.js")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[hash].js",
+    publicPath: "./",
+    chunkFilename: "js/[id].[chunkhash].js"
+  },
+  optimization: {
+    minimizer: [new TersetJSPlugin(), new OptimizeCSSAssetsPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10,
+            name: "[hash].[ext]",
+            outputPath: "assets"
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[hash].css",
+      chunkFilename: "css/[id].[hash].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    }),
+
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/app.*"]
+    })
+  ]
+};
